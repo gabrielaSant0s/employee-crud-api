57cfb4c9213fd3bf2d9a6e3200259f35dc2b1f14
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-19 17:02:12 -0300

    mudei o tamanho do salario no script e no pgadmin

--- sql/script-employee.sql
+++ sql/script-employee.sql
@@ -2,7 +2,7 @@ CREATE TABLE employee (
     employee_id uuid PRIMARY KEY NOT NULL DEFAULT uuii_gerenate_v4(),
     name VARCHAR(255) NOT NULL,
     job_role VARCHAR(255) NOT NULL,
-    salary NUMERIC(5,2),
+    salary NUMERIC(7,2),
     date_birthday DATE NOT NULL,
     employee_registration INTEGER NOT NULL
 )
