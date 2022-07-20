79b95461b37c7593b422645677cb5d1469a66a77
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-19 21:51:26 -0300

    rota listar funcionário por id

--- api-employee/src/controllers/employee.controller.js
+++ api-employee/src/controllers/employee.controller.js
@@ -19,3 +19,9 @@ exports.listAllEmployees = async(req, res) => {
   const response = await db.query("SELECT * FROM employee ORDER BY name ASC")
   res.status(200).send(response.rows)
 };
+
+exports.findEmployeeById = async(req, res) => {
+  const employeeId = req.params.id
+  const response = await db.query("SELECT * FROM employee WHERE employee_id = $1", [employeeId])
+  res.status(200).send(response.rows);
+}
--- api-employee/src/routes/employe.routes.js
+++ api-employee/src/routes/employe.routes.js
@@ -3,5 +3,6 @@ const employeeController = require("../controllers/employee.controller")
 
 router.post("/employees", employeeController.createEmployee)
 router.get("/employees", employeeController.listAllEmployees )
+router.get("/employees/:id", employeeController.findEmployeeById)
 
 module.exports = router;
