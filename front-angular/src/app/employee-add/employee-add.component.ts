998f695b7974811d37781ef3794c5c043b0a9a39
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-21 00:39:46 -0300

    sweetalert ativado

--- front-angular/src/app/employee-add/employee-add.component.ts
+++ front-angular/src/app/employee-add/employee-add.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EmployeesService } from '../employees.service'
 
+import  Swal  from 'sweetalert2';
+
 @Component({
   selector: 'app-employee-add',
   templateUrl: './employee-add.component.html',
@@ -27,6 +29,15 @@ export class EmployeeAddComponent implements OnInit {
 
   createNewEmployee(name: any, job_role: any, salary: any, date_birthday: any, employee_registration: any){
     this.employeeService.createNewEmployee(name, job_role, salary, date_birthday, employee_registration)
+
+    Swal.fire({
+      title: 'Employee added successfully!',
+      icon: 'success',
+      timer: 1500,
+      showConfirmButton: true
+    });
+
+    this.employeeForm.reset();
   }
 
   ngOnInit(): void {
